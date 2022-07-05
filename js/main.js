d2e5fe72593ba2d841187763496e40bc72451b2e
Christian Kessler <96510238+christiankesslers@users.noreply.github.com>
2022-07-05 10:27:55 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -315,3 +315,22 @@
     })();
 
 })(document.documentElement);
+
+let element = document.querySelector('#headline');
+
+function animate(element, duration) {
+  let array = element.innerText;
+  element.innerHTML = '';
+  let index = 0;
+  let interval = setInterval(function () {
+    // element.classList[element.classList.contains('active') ? 'remove' : 'add']('active');
+    element.innerText += array[index];
+    if (array.length - 1 == index) {
+      clearInterval(interval);
+      // element.classList.remove('active');
+    }
+    index++;
+  }, duration);
+}
+
+animate(element, 200);
